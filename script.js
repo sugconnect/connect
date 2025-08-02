eb543c09908cbe8a351c16ce1f4c21a9b4568474
sugconnect <sugconnect@gmail.com>
2025-08-02 13:37:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -265,3 +265,14 @@ document.addEventListener("DOMContentLoaded", () => {
     img.style.transition = "opacity 0.3s ease"
   })
 })
+window.addEventListener('load', () => {
+  const splash = document.getElementById('splash-screen');
+  const mainContent = document.getElementById('main-content');
+
+setTimeout(() => {
+  splash.classList.add('fade-out');
+  setTimeout(() => {
+    splash.style.display = 'none';
+    mainContent.style.display = 'block';
+  }, 3000); // match the transition duration
+}, 2000);
